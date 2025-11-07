9275b4f89525b9f138a6848f119b02f491eb66ce
nimesh-korat <nimeshkorat611@gmail.com>
2025-11-07 10:12:14 +0530

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,149 @@
+// Questions array
+const questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Which of the following is a programming language?",
+    answer: "Python",
+    options: ["HTTP", "FTP", "Python", "HTML"],
+  },
+  {
+    numb: 4,
+    question: "Which tag is used to insert an image in HTML?",
+    answer: "&lt img &gt",
+    options: ["&ltimage&gt", "&ltimg&gt", "&ltsrc&gt", "&ltpicture&gt"],
+  },
+  {
+    numb: 5,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Statement Question Language",
+      "Structured Query Language",
+      "Simple Query Language",
+    ],
+  },
+  {
+    numb: 6,
+    question: "Which language is used for web page styling?",
+    answer: "CSS",
+    options: ["HTML", "CSS", "Python", "PHP"],
+  },
+  {
+    numb: 7,
+    question: "Which symbol is used for comments in C language?",
+    answer: "//",
+    options: ["#", "//", "/*", "&lt!--"],
+  },
+  {
+    numb: 8,
+    question: "Which data type is used to store a single character in C?",
+    answer: "char",
+    options: ["string", "char", "int", "float"],
+  },
+  {
+    numb: 9,
+    question: "Which function is used to display output in C?",
+    answer: "printf()",
+    options: ["print()", "display()", "printf()", "cout"],
+  },
+  {
+    numb: 10,
+    question: "Which of the following is not a database?",
+    answer: "HTML",
+    options: ["MySQL", "MongoDB", "HTML", "Oracle"],
+  },
+  {
+    numb: 11,
+    question: "Which of the following is used to connect to a database in PHP?",
+    answer: "mysqli_connect()",
+    options: [
+      "db_connect()",
+      "mysqli_connect()",
+      "sql_connect()",
+      "connect_db()",
+    ],
+  },
+  {
+    numb: 12,
+    question: "Which tag is used to create a hyperlink in HTML?",
+    answer: "&lta&gt",
+    options: ["&ltlink&gt", "&lta&gt", "&lthref&gt", "&lturl&gt"],
+  },
+  {
+    numb: 13,
+    question: "What is the full form of URL?",
+    answer: "Uniform Resource Locator",
+    options: [
+      "Uniform Resource Locator",
+      "Universal Reference Link",
+      "Uniform Reference Locator",
+      "Universal Resource Link",
+    ],
+  },
+  {
+    numb: 14,
+    question:
+      "Which JavaScript method is used to print content in the console?",
+    answer: "console.log()",
+    options: ["print()", "display()", "console.log()", "log.print()"],
+  },
+  {
+    numb: 15,
+    question: "Which keyword is used to define a variable in JavaScript?",
+    answer: "let",
+    options: ["define", "new", "let", "varies"],
+  },
+  {
+    numb: 16,
+    question: "Which of the following is an example of an operating system?",
+    answer: "Linux",
+    options: ["MS Word", "Google", "Linux", "HTML"],
+  },
+  {
+    numb: 17,
+    question: "Which of the following is not a front-end technology?",
+    answer: "Python",
+    options: ["HTML", "CSS", "JavaScript", "Python"],
+  },
+  {
+    numb: 18,
+    question: "Which protocol is used to transfer web pages?",
+    answer: "HTTP",
+    options: ["SMTP", "HTTP", "FTP", "TCP"],
+  },
+  {
+    numb: 19,
+    question: "Which of the following is used to store data permanently?",
+    answer: "Hard Disk",
+    options: ["RAM", "Cache", "Register", "Hard Disk"],
+  },
+  {
+    numb: 20,
+    question: "Which tag is used to create a table in HTML?",
+    answer: "&lttable&gt",
+    options: ["&lttbl&gt", "&lttable&gt", "&lttr&gt", "&lttd&gt"],
+  },
+];
